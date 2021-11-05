1e38424cb4ef64d4b305e82dfc9ee9d625662cbd
Luis Pflamminger <luis.pflamminger@gmail.com>
2021-11-05 17:58:19 +0100

    refactoring

--- main.py
+++ main.py
@@ -1,36 +1,15 @@
 import json
 import os
+import argparse
+import sys
+import mechanicalsoup
 
 SUPPORTED_GPUS = ["3060", "3060Ti", "3070", "3080"]
 BASE_URL = "https://www.ebay-kleinanzeigen.de/"
 
 def main():
-    args = parse_args()
-
-    #UI not implemented yet
-    if args.ui == True:
-        print("UI not implemented yet. Please use cli options.")
-        return
-
-    #User entered incorrect GPU
-    for el in args.gpus:
-        if el not in SUPPORTED_GPUS:
-            print("You entered an unsupported GPU Type. Here is a list of supported values") 
-            print(SUPPORTED_GPUS)
-            #return
     
-    data = args.gpus
-
-    for el in data:
-        scrape(el, args.pages, args.zip_code, args.radius)
-        el = []
-
-
-
-
-def parse_args():
-    import argparse
-
+    ### parse arguments ###    
     parser = argparse.ArgumentParser(description='Ebay Kleinanzeigen GPU Deal Finder', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-u', '--ui', action="store_true", help='Choose the configuration using a user interface instead of cli options. (Recommended for manual usage and beginners)')
     parser.add_argument('-g', '--gpus', nargs="+", default=SUPPORTED_GPUS, help='The GPU models you want to include in your search. Default is all supported models.')
@@ -40,22 +19,40 @@ def parse_args():
     parser.add_argument('-r', '--radius', default=None, type=int, help='Specify a search radius.')
 
     args = parser.parse_args()
-    print(args)
-    return args
 
-def scrape(gpu, pages, zip_code, radius):
-    import mechanicalsoup
+
+    ### Handle input ###
     
-    browser = mechanicalsoup.StatefulBrowser(
-        soup_config={'features': 'html5lib'},
-        raise_on_404=True,
-        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
-    )
+    # UI not implemented yet
+    if args.ui == True:
+        print("UI not implemented yet. Please use cli options.")
+        return
 
-    browser.open(BASE_URL)
-    #browser.select_form('#site-search-form')
-    print(browser.page.find_all("form"))
-    browser.close()
+    #User entered incorrect GPU
+    for el in args.gpus:
+        if el not in SUPPORTED_GPUS:
+            sys.exit("You entered an unsupported GPU Type. Here is a list of supported values:\n" + SUPPORTED_GPUS)
+    
+    pages = args.pages
+    zip_code = args.zip_code
+    radius = args.radius
+    gpus = args.gpus
+
+
+    ### Start scraping ###
+
+    for gpu in gpus:
+        browser = mechanicalsoup.StatefulBrowser(
+            soup_config={'features': 'html5lib'},
+            raise_on_404=True,
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+        )
+
+        browser.open(BASE_URL)
+        #browser.select_form('#site-search-form')
+        print(browser.page.find_all("form"))
+        browser.close()
+
 
 if __name__ == "__main__":
     main()
